71f7d0d80ef00af0dbe4e5ac93201b994a681ef5
sayansgithub <161965997+sayansgithub@users.noreply.github.com>
2024-03-02 12:34:31 -0500

    Add files via upload

---
+++ 1.6cw.py
@@ -0,0 +1,151 @@
+import turtle
+screen = turtle.Screen() 
+screen.bgcolor("light blue")
+screen.setup(800, 500) 
+
+tom=turtle.Turtle()
+tom.penup()
+tom.goto(0,-300)
+tom.pendown()
+tom.color('red')
+tom.begin_fill()
+tom.circle(200)
+tom.end_fill()
+
+tom.color('orange')
+tom.begin_fill()
+tom.circle(180)
+tom.end_fill()
+
+tom.color('yellow')
+tom.begin_fill()
+tom.circle(160)
+tom.end_fill()
+
+tom.color('green')
+tom.begin_fill()
+tom.circle(140)
+tom.end_fill()
+
+tom.color('blue')
+tom.begin_fill()
+tom.circle(120)
+tom.end_fill()
+
+tom.color('purple')
+tom.begin_fill()
+tom.circle(100)
+tom.end_fill()
+
+tom.color('light blue')
+tom.begin_fill()
+tom.circle(80)
+tom.end_fill()
+
+screen.reset()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+##sasha = turtle.Turtle() # creating a turtle
+##screen = turtle.Screen() # creating a screen
+##screen.bgcolor("yellow") # make the screen yellow
+##sasha.forward(100) # sasha draws a triangle
+##sasha.right(120)
+##sasha.forward(100)
+##sasha.right(120)
+##sasha.forward(100)
+##screen.reset() # the triangle is gone!
+##sasha.forward(100)
+##screen.clear() 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+##screen = turtle.Screen() # this is like creating a turtle
+### but this time we are creating a Screen
+##screen.bgcolor("blue") # set the colour
+##screen.setup(600, 300) # se
+##
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+##import turtle
+##kayla = turtle.Turtle()
+##obie = turtle.Turtle()
+##john = turtle.Turtle()
+##holly = turtle.Turtle() # we made 4 different turtles!
+##kayla.color("red")
+##obie.color("green")
+##john.color("purple")
+##holly.color("orange") # they all change colour one by one
+##kayla.speed(1)
+##obie.speed(0)
+##john.speed(4) # who is the fastest?
+##kayla.goto(0,100) # remember week 4 - all about the grid!
+##obie.goto(100, 0) # where will they end up?
+##john.goto(-100, 0)
+##holly.goto(0, -100)
+##kayla.hideturtle() # kayla will disappear!
+
---
+++ 1.9 test.py
@@ -0,0 +1,390 @@
+import turtle
+tom = turtle.Turtle()
+screen = turtle.Screen()
+tom.speed(0)
+screen.bgcolor('light blue')
+screen.setup(800,500)
+tom.penup()
+tom.goto(-400,-100)
+tom.pendown()
+tom.color('light yellow')
+tom.begin_fill()
+tom.forward(800)
+tom.right(90)
+tom.forward(150)
+tom.right(90)
+tom.forward(800)
+tom.right(90)
+tom.forward(150)
+tom.right(90)
+tom.end_fill()
+##see weed
+tom.color('green')
+tom.pensize(5)
+tom.setheading(90)
+
+
+tom.penup()
+tom. goto(375,-150)
+tom.pendown()
+tom.forward(100)
+
+tom.penup()
+tom. goto(360,-160)
+tom.pendown()
+tom.forward(100)
+
+tom.penup()
+tom. goto(340,-140)
+tom.pendown()
+tom.forward(100)
+
+tom.penup()
+tom. goto(300,-120)
+tom.pendown()
+tom.forward(100)
+
+tom.penup()
+tom. goto(240,-160)
+tom.pendown()
+tom.forward(100)
+
+tom.penup()
+tom. goto(-375,-150)
+tom.pendown()
+tom.forward(100)
+
+tom.penup()
+tom. goto(-360,-160)
+tom.pendown()
+tom.forward(100)
+
+tom.penup()
+tom. goto(-340,-140)
+tom.pendown()
+tom.forward(100)
+
+tom.penup()
+tom. goto(-300,-120)
+tom.pendown()
+tom.forward(100)
+
+tom.penup()
+tom. goto(-240,-160)
+tom.pendown()
+tom.forward(100)
+
+tom.color('light green')
+
+tom.penup()
+tom. goto(320,-150)
+tom.pendown()
+tom.forward(100)
+
+tom.penup()
+tom. goto(220,-160)
+tom.pendown()
+tom.forward(100)
+
+tom.penup()
+tom. goto(260,-140)
+tom.pendown()
+tom.forward(100)
+
+tom.penup()
+tom. goto(280,-120)
+tom.pendown()
+tom.forward(100)
+
+tom.penup()
+tom. goto(390,-160)
+tom.pendown()
+tom.forward(100)
+
+tom.penup()
+tom. goto(-320,-150)
+tom.pendown()
+tom.forward(100)
+
+tom.penup()
+tom. goto(-220,-160)
+tom.pendown()
+tom.forward(100)
+
+tom.penup()
+tom. goto(-260,-140)
+tom.pendown()
+tom.forward(100)
+
+tom.penup()
+tom. goto(-280,-120)
+tom.pendown()
+tom.forward(100)
+
+tom.penup()
+tom. goto(-390,-160)
+tom.pendown()
+tom.forward(100)
+
+##Corals
+tom.penup()
+tom.goto(0,-150)
+tom.pendown()
+tom.setheading(90)
+tom.pencolor('pink')
+tom.fillcolor('red')
+tom.begin_fill()
+tom.pensize(5)
+tom.forward(100)
+tom.left(60)
+tom.forward(30)
+tom.right(90)
+tom.forward(30)
+tom.right(90)
+tom.forward(30)
+tom.left(120)
+tom.forward(50)
+tom.right(90)
+tom.forward(30)
+tom.right(90)
+tom.forward(80)
+tom.left(120)
+tom.forward(30)
+tom.right(90)
+tom.forward(30)
+
+tom.right(90)
+tom.forward(40)
+tom.left(60)
+tom.forward(50)
+tom.end_fill()
+
+
+
+
+
+
+tom.penup()
+tom.goto(-150,-150)
+tom.pendown()
+tom.setheading(90)
+tom.pencolor('magenta')
+tom.fillcolor('purple')
+tom.begin_fill()
+tom.pensize(5)
+tom.forward(100)
+tom.left(60)
+tom.forward(30)
+tom.right(90)
+tom.forward(30)
+tom.right(90)
+tom.forward(30)
+tom.left(120)
+tom.forward(50)
+tom.right(90)
+tom.forward(30)
+tom.right(90)
+tom.forward(80)
+tom.left(120)
+tom.forward(30)
+tom.right(90)
+tom.forward(30)
+
+tom.right(90)
+tom.forward(40)
+tom.left(60)
+tom.forward(50)
+tom.end_fill()
+
+
+
+
+
+tom.penup()
+tom.goto(150,-150)
+tom.pendown()
+tom.setheading(90)
+tom.pencolor('yellow')
+tom.fillcolor('orange')
+tom.begin_fill()
+tom.pensize(5)
+tom.forward(100)
+tom.left(60)
+tom.forward(30)
+tom.right(90)
+tom.forward(30)
+tom.right(90)
+tom.forward(30)
+tom.left(120)
+tom.forward(50)
+tom.right(90)
+tom.forward(30)
+tom.right(90)
+tom.forward(80)
+tom.left(120)
+tom.forward(30)
+tom.right(90)
+tom.forward(30)
+
+tom.right(90)
+tom.forward(40)
+tom.left(60)
+tom.forward(50)
+tom.end_fill()
+
+
+
+
+
+##fish
+
+
+tom.penup()
+tom.goto(230,150)
+tom.setheading(10)
+tom.pendown()
+
+tom.pencolor('blue')
+tom.pensize(5)
+tom.fillcolor('aqua')
+tom.begin_fill()
+tom.forward(50)
+tom.left(120)
+tom.forward(50)
+tom.left(120)
+tom.forward(50)
+tom.left(120)
+tom.end_fill()
+tom.penup()
+tom.goto(245,155)
+tom.pensize(10)
+tom.setheading(90)
+tom.pendown()
+tom.fillcolor('blue')
+tom.begin_fill()
+tom.circle(25)
+tom.end_fill()
+tom.circle(25,160)
+tom.fillcolor('black')
+tom.pensize(10)
+tom.begin_fill()
+tom.circle(11)
+tom.end_fill()
+
+
+
+tom.penup()
+tom.goto(-200,100)
+tom.setheading(270)
+tom.pendown()
+
+tom.pencolor('pink')
+tom.pensize(5)
+tom.fillcolor('magenta')
+tom.begin_fill()
+tom.forward(50)
+tom.left(120)
+tom.forward(50)
+tom.left(120)
+tom.forward(50)
+tom.left(120)
+tom.end_fill()
+tom.penup()
+tom.goto(-180,90)
+tom.pensize(10)
+tom.setheading(60)
+tom.pendown()
+tom.fillcolor('pink')
+tom.begin_fill()
+tom.circle(25)
+tom.end_fill()
+tom.circle(25,95)
+tom.fillcolor('black')
+tom.pensize(10)
+tom.begin_fill()
+tom.circle(11)
+tom.end_fill()
+
+tom.penup()
+tom.goto(-200,160)
+tom.pensize(5)
+tom.pendown()
+tom.color('white')
+tom.setheading(90)
+tom.circle(15)
+
+tom.penup()
+tom.goto(-160,180)
+tom.pensize(5)
+tom.pendown()
+tom.color('white')
+tom.setheading(90)
+tom.circle(15)
+
+tom.penup()
+tom.goto(-123,200)
+tom.pensize(5)
+tom.pendown()
+tom.color('white')
+tom.setheading(90)
+tom.circle(15)
+
+
+
+
+a = turtle.Turtle()
+b = turtle.Turtle()
+c = turtle.Turtle()
+d = turtle.Turtle()
+
+
+
+
+a = turtle.Turtle()
+b = turtle.Turtle()
+c = turtle.Turtle()
+d = turtle.Turtle()
+
+
+a.penup()
+a.goto(370,-230)
+a.right(180)
+a.shape('turtle')
+
+b.penup()
+b.goto(350,-230)
+b.right(180)
+b.shape('turtle')
+
+c.penup()
+c.goto(330,-230)
+c.right(180)
+c.shape('turtle')
+
+d.penup()
+d.goto(310,-230)
+d.right(180)
+d.shape('turtle')
+
+
+
+
+for i in range(100):
+    d.forward(15)
+    c.forward(15)
+    b.forward(15)
+    a.forward(15)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
